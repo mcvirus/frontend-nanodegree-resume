d121725faf0f415210d80a59e39293086d6fc3e0
mcvirus <vortaxg@gmail.com>
2015-02-27 12:09:31 +0200

    Merge branch 'master' of github.com:udacity/frontend-nanodegree-resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,3 +4,19 @@ console.log(awesomeThoughts);
 var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
 console.log(funThoughts);
 $("#main").append(funThoughts);
+
+var name = "Vitaliy";
+var formattedName = HTMLheaderName.replace("%data%", name);
+$("#header").append(formattedName);
+
+var role = "Front-end Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").append(formattedRole);
+
+var bio = {
+    "name" : "Vitaliy",
+    "role" : "Front-end Developer",
+    "contact" : "foo",
+    "welcomeMessage" : "Welcome to my site",
+    "skills" : "foo"
+};
