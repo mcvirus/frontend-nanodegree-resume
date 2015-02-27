69b3f2da34da2d3a27c8945ef5a85ed26992aa3b
mcvirus <vortaxg@gmail.com>
2015-02-27 13:40:43 +0200

    Merge branch 'master' of github.com:udacity/frontend-nanodegree-resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,18 +5,46 @@ var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
 console.log(funThoughts);
 $("#main").append(funThoughts);
 
-var name = "Vitaliy";
-var formattedName = HTMLheaderName.replace("%data%", name);
-$("#header").append(formattedName);
-
-var role = "Front-end Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-$("#header").append(formattedRole);
 
 var bio = {
     "name" : "Vitaliy",
     "role" : "Front-end Developer",
-    "contact" : "foo",
-    "welcomeMessage" : "Welcome to my site",
-    "skills" : "foo"
+    "contacts": {
+    "mobile" : "123-456-789",
+    "email" : "anonymous@gmail.com",
+    "twitter" : "@anonymous",
+    "github": "anonymous",
+    "blog" : "anonymous.blogspot.com",
+    "location" : "someCity"
+ },
+    "welcomeMsg" : "WELCOME",
+    "skills" :["awesomeness", "funny", "clever"],
+    "picture" : "images/fry.jpg"
 };
+
+
+
+$("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
+
+$("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+
+$("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
+
+$("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
+
+$("#topContacts").append(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+
+$("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
+
+$("#topContacts").append(HTMLblog.replace("%data%", bio.contacts.blog));
+
+$("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
+
+$("#header").append(HTMLWelcomeMsg.replace("%data%", bio.welcomeMsg));
+
+$("#header").append(HTMLbioPic.replace("%data%",bio.picture));
+
+$("#header").append(HTMLskillsStart);
+$("#header").append(HTMLskills.replace("%data%", bio.skills[0]));
+$("#header").append(HTMLskills.replace("%data%", bio.skills[1]));
+$("#header").append(HTMLskills.replace("%data%", bio.skills[2]));
